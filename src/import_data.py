4efea325ec9324bfde8b4531360bed358fb102e2
LuiAr <louis.arbey@efrei.net>
2024-01-28 15:48:54 +0100

    Revert "Add print statement to import_data.py"
    This reverts commit 1ea8f20de208f1c2e1ef6fd077bbaab8cc9c3ebb.

--- src/import_data.py
+++ src/import_data.py
@@ -1,3 +1,2 @@
 # Python file to import the data from the API, clean it, and store in our DB
 
-print("Hello")
--- src/test/test_import_data.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import import_data
-
-class TestImportData(unittest.TestCase):
-    def test_print_hello(self):
-        self.assertEqual(1, 1)
-
-if __name__ == '__main__':
-    unittest.main()
