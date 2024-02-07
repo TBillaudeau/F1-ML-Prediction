116a443949c8194a371d32ef3173e060392c0a16
LuiAr <louis.arbey@efrei.net>
2024-02-07 10:37:01 +0100

    FIxed driverid when importing data

--- src/import_data.py
+++ src/import_data.py
@@ -15,7 +15,8 @@ def get_most_recent_dir():
 def import_files(most_recent_dir):
     last_results = pd.read_csv(os.path.join(most_recent_dir + "/results.csv"))
     last_races = pd.read_csv(os.path.join(most_recent_dir + "/races.csv"))
-    return last_results, last_races
+    last_drivers = pd.read_csv(os.path.join(most_recent_dir + "/drivers.csv"))
+    return last_results, last_races, last_drivers
 
 def get_api_data():
     url = 'http://ergast.com/api/f1/current/last/results.json'
@@ -37,7 +38,7 @@ def check_new_data(results, raceName, last_results, last_races):
     print("New data available.")
     return True
 
-def save_new_data(results, raceName, last_results, last_races):
+def save_new_data(results, raceName, last_results, last_races,last_drivers):
     last_resultId = last_results.iloc[-1]['resultId']
     last_resultId += 1
     raceId = last_races[last_races['name'] == raceName].iloc[-1]['raceId']
@@ -45,44 +46,53 @@ def save_new_data(results, raceName, last_results, last_races):
 
     for race in results:
         for result in race['Results']:
-            for result in race['Results']:
-                driverId = result['Driver'].get('driverId', None)
-                constructorId = result['Constructor'].get('constructorId', None)
-                driverNumber = result.get('number', None)
-                gridPosition = result.get('grid', None)
-                positionOrder = result.get('position', None)
-                points = result.get('points', None)
-                laps = result.get('laps', None)
-                time = result['Time'].get("time", None) if 'Time' in result else None
-                milliseconds = result['Time'].get("millis", None) if 'Time' in result else None
-                fastestLap = result['FastestLap'].get("lap", None) if 'FastestLap' in result else None
-                rankFastestLap = result['FastestLap'].get("rank", None) if 'FastestLap' in result else None
-                fastestLapTime = result['FastestLap']['Time'].get("time", None) if 'FastestLap' in result and 'Time' in result['FastestLap'] else None
-                fastestLapSpeed = result['FastestLap']['AverageSpeed'].get("speed", None) if 'FastestLap' in result and 'AverageSpeed' in result['FastestLap'] else None
-                status = result.get('status', None)
+            code = result['Driver'].get('code', None)
+            permanentNumber = result['Driver'].get('permanentNumber', None)
+            constructorId = result['Constructor'].get('constructorId', None)
+            driverNumber = result.get('number', None)
+            gridPosition = result.get('grid', None)
+            positionOrder = result.get('position', None)
+            points = result.get('points', None)
+            laps = result.get('laps', None)
+            time = result['Time'].get("time", None) if 'Time' in result else None
+            milliseconds = result['Time'].get("millis", None) if 'Time' in result else None
+            fastestLap = result['FastestLap'].get("lap", None) if 'FastestLap' in result else None
+            rankFastestLap = result['FastestLap'].get("rank", None) if 'FastestLap' in result else None
+            fastestLapTime = result['FastestLap']['Time'].get("time", None) if 'FastestLap' in result and 'Time' in result['FastestLap'] else None
+            fastestLapSpeed = result['FastestLap']['AverageSpeed'].get("speed", None) if 'FastestLap' in result and 'AverageSpeed' in result['FastestLap'] else None
+            status = result.get('status', None)
 
-                data.append({
-                    'resultId': last_resultId,
-                    'raceId': raceId,
-                    'driverId': driverId,
-                    'constructorId': constructorId,
-                    'number': driverNumber,
-                    'grid': gridPosition,
-                    'position': positionOrder,
-                    'positionText': positionOrder,
-                    'positionOrder': positionOrder,
-                    'points': points,
-                    'laps': laps,
-                    'time': time,
-                    'milliseconds': milliseconds,
-                    'fastestLap': fastestLap,
-                    'rank': rankFastestLap,
-                    'fastestLapTime': fastestLapTime,
-                    'fastestLapSpeed': fastestLapSpeed,
-                    'statusId': status
-                })
+            # Get driverId from last_drivers and match code with code AND permanentNumber with number
+            driver_df = last_drivers[(last_drivers['code'] == code) & (last_drivers['number'] == permanentNumber)]
+            if not driver_df.empty:
+                driverId = driver_df.iloc[0]['driverId']
+            else:
+                print(f"No driver found with code {code} and number {permanentNumber}, setting to 0")
+                driverId = 0
+                continue
 
-                last_resultId += 1
+            data.append({
+                'resultId': last_resultId,
+                'raceId': raceId,
+                'driverId': driverId,
+                'constructorId': constructorId,
+                'number': driverNumber,
+                'grid': gridPosition,
+                'position': positionOrder,
+                'positionText': positionOrder,
+                'positionOrder': positionOrder,
+                'points': points,
+                'laps': laps,
+                'time': time,
+                'milliseconds': milliseconds,
+                'fastestLap': fastestLap,
+                'rank': rankFastestLap,
+                'fastestLapTime': fastestLapTime,
+                'fastestLapSpeed': fastestLapSpeed,
+                'statusId': status
+            })
+
+            last_resultId += 1
 
     df = pd.DataFrame(data)
     last_results = pd.concat([last_results, df], ignore_index=True)
@@ -92,13 +102,14 @@ def save_new_data(results, raceName, last_results, last_races):
     last_results.to_csv(f'{new_dir}/results.csv', index=False)
     print(f"\n--> Results saved in {new_dir} directory")
     last_races.to_csv(f'{new_dir}/races.csv', index=False)
+    last_drivers.to_csv(f'{new_dir}/drivers.csv', index=False)
 
 def main():
     most_recent_dir = get_most_recent_dir()
-    last_results, last_races = import_files(most_recent_dir)
+    last_results, last_races,last_drivers = import_files(most_recent_dir)
     results, raceName = get_api_data()
     if check_new_data(results, raceName, last_results, last_races):
-        save_new_data(results, raceName, last_results, last_races)
+        save_new_data(results, raceName, last_results, last_races,last_drivers)
 
 if __name__ == '__main__':
     main()
