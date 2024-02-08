f4943881f1c40a49eb936e58295ba71dc770b74b
Pierre-Louis Cretinon <pierre-louis.cretinon@efrei.net>
2024-02-08 14:39:18 +0100

    Refactor test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -1,37 +1,44 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.chrome.options import Options
+# import chromedriver_autoinstaller_fix
 
-def test_main():
-    # Create a new instance of the Firefox driver
-    options = Options()
-    options.add_argument("--headless")
 
-    driver = webdriver.Firefox(options=options)
+# import time
 
-    # Go to your app's URL
-    driver.get("https://efrei--streamlit--4f4tjmnqvxl8.code.run/")  # replace with your app's URL
+# def test_main():
+#     # Create a new instance of the Firefox driver
+#     chrome_options = Options()
+#     chrome_options.add_argument("--headless")
+#     chrome_options.add_argument("--no-sandbox")
+#     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    # Wait for the predict button to be present
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "Predict Finishing Position 🚀")]')))
+#     chromedriver_autoinstaller_fix.install()  
+#     driver = webdriver.Chrome(options=chrome_options)
 
-    # Test the title
-    assert "F1 Grand Prix Prediction" in driver.page_source
+#     # Go to your app's URL
+#     driver.get("https://efrei--streamlit--4f4tjmnqvxl8.code.run/")  # replace with your app's URL
 
-    # Test the sidebar
-    assert "Select a model" in driver.page_source
+#     # Wait for the predict button to be present
+#     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(text(), "Predict Finishing Position 🚀")]')))
 
-    # Test the user input
-    assert "Select Constructor" in driver.page_source
-    assert "Select Driver" in driver.page_source
+#     # Test the title
+#     assert "F1 Grand Prix Prediction" in driver.page_source
 
-    # Test the predict button
-    assert "Predict Finishing Position 🚀" in driver.page_source
+#     # Test the sidebar
+#     assert "Select a model" in driver.page_source
 
-    # Close the browser window
-    driver.quit()
+#     # Test the user input
+#     assert "Select Constructor" in driver.page_source
+#     assert "Select Driver" in driver.page_source
 
-if __name__ == "__main__":
-    test_main()
+#     # Test the predict button
+#     assert "Predict Finishing Position 🚀" in driver.page_source
+
+#     # Close the browser window
+#     driver.quit()
+
+# if __name__ == "__main__":
+#     test_main()
